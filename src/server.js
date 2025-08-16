ac1f43fd4a39e1427fab6a52bbd556ca7c656567
Gabriel Niz <gabrielniz@MacBook-Pro-de-Gabriel.local>
2025-08-16 20:39:41 -0300

    creating the GET resource for users

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
-import http from 'http'
+import http from 'node:http'
+
+const user = [{"name": "Gabriel Niz", "email": "gabriel@email.com"}]
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Hello, World!\n')})
-
-
+  
+    if (req.method === 'GET' && req.url === '/users') {
+        res.writeHead(200, { 'Content-Type': 'aplication/json' })
+        res.end(JSON.stringify(user))
+}})
   server.listen(3333, () => {
-    console.log('server is running on port 3333')
+    console.log('🔥 Server is running at http://localhost:3333')
   })
