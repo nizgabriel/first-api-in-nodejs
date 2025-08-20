bdfbe9749c7ae1077e33c21991bd02256e2a4dab
Gabriel Niz <gabrielniz@MacBook-Pro-de-Gabriel.local>
2025-08-19 23:00:31 -0300

    http code

--- src/server.js
+++ src/server.js
@@ -1,13 +1,30 @@
 import http from 'node:http'
 
-const user = [{"name": "Gabriel Niz", "email": "gabriel@email.com"}]
+
+const users = []
 
 const server = http.createServer((req, res) => {
   
-    if (req.method === 'GET' && req.url === '/users') {
-        res.writeHead(200, { 'Content-Type': 'aplication/json' })
-        res.end(JSON.stringify(user))
-}})
+  const {method, url} = req
+  
+    if (method === 'GET' && url === '/users') {
+      return res
+        .setHearder ('Content-Type', 'aplication/json')
+        .end(JSON.stringify(users))
+}
+    if (method === 'POST' && url === '/users') {
+      users.push({
+        id: 1,
+        name: 'John Doe',
+        email: "johndoe@email.com"
+      })
+
+      return  res.end("Criacao de usuario")
+    }
+    return res.end('Hello World')
+  })
+
+
   server.listen(3333, () => {
     console.log('🔥 Server is running at http://localhost:3333')
   })
